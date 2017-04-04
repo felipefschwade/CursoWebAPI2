2fadca9d03e84c98a84c7a20708f871ba6e5bb8e
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-04-04 15:29:14 -0400

    Atividades da Aula 1 - Criando um controlador que retorna um carrinho em XML

---
+++ LojaAPI/Controllers/CarrinhoController.cs
@@ -0,0 +1,20 @@
+﻿using Loja.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaAPI.Controllers
+{
+    public class CarrinhoController : ApiController
+    {
+        public string Get(long id)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            var carrinho = dao.Busca(id);
+            return carrinho.ToXml();
+        }
+    }
+}
