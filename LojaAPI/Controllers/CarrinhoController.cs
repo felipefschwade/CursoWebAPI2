36724bce09efbd56a222209e474d0cfe904c7754
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-04-06 16:24:27 -0400

    Adicionando uma resposta de erro

--- LojaAPI/Controllers/CarrinhoController.cs
+++ LojaAPI/Controllers/CarrinhoController.cs
@@ -1,4 +1,5 @@
 ﻿using Loja.DAO;
+using Loja.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,31 @@ namespace LojaAPI.Controllers
 {
     public class CarrinhoController : ApiController
     {
-        public string Get(long id)
+        public HttpResponseMessage Get(long id)
+        {
+            try
+            {
+                CarrinhoDAO dao = new CarrinhoDAO();
+                var carrinho = dao.Busca(id);
+                return Request.CreateResponse(HttpStatusCode.OK, carrinho);
+            }
+            catch (Exception)
+            {
+                var msg = string.Format("O carrinho {0} não foi encontrado", id);
+                var httperror = new HttpError(msg);
+                return Request.CreateResponse(HttpStatusCode.NotFound, httperror);
+            }            
+        }
+        
+        public HttpResponseMessage Post([FromBody] Carrinho carrinho)
         {
             CarrinhoDAO dao = new CarrinhoDAO();
-            var carrinho = dao.Busca(id);
-            return carrinho.ToXml();
+            dao.Adiciona(carrinho);
+            HttpResponseMessage resp = Request.CreateResponse(HttpStatusCode.Created);
+            string location = Url.Link("DefaultApi", new { controller = "carrinho", id = carrinho.Id });
+            resp.Headers.Location = new Uri(location);
+            return resp;
         }
+
     }
 }
